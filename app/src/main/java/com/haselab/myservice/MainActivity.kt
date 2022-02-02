3584b44c0084ce74a2b9d132397cea713f707634
hasegawa tadamitsu <hasegawa.tadamitsu@gmail.com>
2022-02-03 06:06:43 +0900

    ok

--- app/src/main/java/com/haselab/myservice/MainActivity.kt
+++ app/src/main/java/com/haselab/myservice/MainActivity.kt
@@ -1,69 +1,109 @@
 package com.haselab.myservice
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+private const val TAG = "MainActivity"
 
 class MainActivity() : AppCompatActivity(), Parcelable {
 
-	constructor(parcel: Parcel) : this() {
-	}
+    constructor(parcel: Parcel) : this()
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+    override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray,
+	) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.v(TAG, "dialog$requestCode $grantResults[0]")
+        if (requestCode != 1000) {
+            Log.v(TAG, "dialog$requestCode not 1000")
+            return
+        }
+        if (grantResults.any { it != PackageManager.PERMISSION_GRANTED }) {
+            Log.v(TAG, " not GRANTED ")
+            AlertDialog.Builder(this)
+                .setMessage(" need location permission")
+                .setPositiveButton("OK", { _, which ->
+                    finish()
+                }).show()
+            return
+        }
+        Log.v("permission", "ok")
+    }
 
-		val fromNotification = intent.getBooleanExtra("fromNotification", false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-		if(fromNotification) {
-			val btPlay = findViewById<Button>(R.id.btPlay)
-			val btStop = findViewById<Button>(R.id.btStop)
-			btPlay.isEnabled = false
-			btStop.isEnabled = true
-		}
-	}
+        val fromNotification = intent.getBooleanExtra("fromNotification", false)
 
-	fun onPlayButtonClick(view: View) {
-		val intent = Intent(this@MainActivity, SampleService::class.java)
+        if (fromNotification) {
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            val btStop = findViewById<Button>(R.id.btStop)
+            btPlay.isEnabled = false
+            btStop.isEnabled = true
+        }
 
-		startService(intent)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.v(TAG, "error  permission")
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions, 1000)
+            return
+        }
 
-		val btPlay = findViewById<Button>(R.id.btPlay)
-		val btStop = findViewById<Button>(R.id.btStop)
-		btPlay.isEnabled = false
-		btStop.isEnabled = true
-	}
+    }
 
-	fun onStopButtonClick(view: View) {
-		val intent = Intent(this@MainActivity, SampleService::class.java)
+    fun onPlayButtonClick(view: View) {
+        val intent = Intent(this@MainActivity, SampleService::class.java)
 
-		stopService(intent)
+        startService(intent)
 
-		val btPlay = findViewById<Button>(R.id.btPlay)
-		val btStop = findViewById<Button>(R.id.btStop)
-		btPlay.isEnabled = true
-		btStop.isEnabled = false
-	}
+        val btPlay = findViewById<Button>(R.id.btPlay)
+        val btStop = findViewById<Button>(R.id.btStop)
+        btPlay.isEnabled = false
+        btStop.isEnabled = true
+    }
 
-	override fun writeToParcel(parcel: Parcel, flags: Int) {
+    fun onStopButtonClick(view: View) {
+        val intent = Intent(this@MainActivity, SampleService::class.java)
 
-	}
+        stopService(intent)
 
-	override fun describeContents(): Int {
-		return 0
-	}
+        val btPlay = findViewById<Button>(R.id.btPlay)
+        val btStop = findViewById<Button>(R.id.btStop)
+        btPlay.isEnabled = true
+        btStop.isEnabled = false
+    }
 
-	companion object CREATOR : Parcelable.Creator<MainActivity> {
-		override fun createFromParcel(parcel: Parcel): MainActivity {
-			return MainActivity(parcel)
-		}
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
 
-		override fun newArray(size: Int): Array<MainActivity?> {
-			return arrayOfNulls(size)
-		}
-	}
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
--- app/src/main/java/com/haselab/myservice/SampleService.kt
+++ app/src/main/java/com/haselab/myservice/SampleService.kt
@@ -1,17 +1,46 @@
 package com.haselab.myservice
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.google.android.gms.location.*
+
+private const val TAG = "Service"
 
 class SampleService : Service() {
     companion object {
         private const val CHANNEL_ID = "SampleService_notification_channel"
+    }
+
+    private var _latitude = 0.0
+    private var _longitude = 0.0
+    private lateinit var _fusedLocationClient: FusedLocationProviderClient
+    private lateinit var _locationRequest: LocationRequest
+    private lateinit var _onUpdateLocation: OnUpdateLocation
+
+    private inner class OnUpdateLocation : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            Log.v(TAG, "start onLocationResult")
+            locationResult.let { itx ->
+                val location = itx.lastLocation
+                location.let {
+                    _latitude = it.latitude
+                    _longitude = it.longitude
+                    val text = _latitude.toString()
+                    val text2 = _longitude.toString()
+                    Log.v(TAG, "latitude = $text")
+                    Log.v(TAG, "longitude = $text2")
+                }
+            }
+        }
     }
 
     override fun onCreate() {
@@ -25,38 +54,63 @@ class SampleService : Service() {
         val manager = getSystemService(NotificationManager::class.java)
         // 通知チャネルを設定。
         manager.createNotificationChannel(channel)
+
+        _fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        _locationRequest = LocationRequest.create()
+        _locationRequest.let {
+            it.interval = 5000 // milli sec
+            it.fastestInterval = 1000 // mill sec
+            it.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        _onUpdateLocation = OnUpdateLocation()
+        Log.v(TAG, "permission check")
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.v(TAG,"permission check error" )
+            msgPermissionError()
+            return
+        }
+        Log.v(TAG, "permission ok")
+        _fusedLocationClient.requestLocationUpdates(_locationRequest, _onUpdateLocation, mainLooper)
     }
 
-    private fun msg() {
-        Log.v("aaa","msg start")
+    private fun msgPermissionError() {
+        Log.v(TAG, "msg permission error start")
 
         // Notificationを作成するBuilderクラス生成。
         val builder = NotificationCompat.Builder(this@SampleService, CHANNEL_ID)
         // 通知エリアに表示されるアイコンを設定。
         builder.setSmallIcon(android.R.drawable.ic_dialog_info)
         // 通知ドロワーでの表示タイトルを設定。
-        builder.setContentTitle(getString(R.string.msg_notification_title_finish))
+        builder.setContentTitle("error Permission")
         // 通知ドロワーでの表示メッセージを設定。
-        builder.setContentText(getString(R.string.msg_notification_text_finish))
+        builder.setContentText("need permission")
         // BuilderからNotificationオブジェクトを生成。
         val notification = builder.build()
         // NotificationManagerCompatオブジェクトを取得。
         val manager = NotificationManagerCompat.from(this@SampleService)
         // 通知。
         manager.notify(100, notification)
-//        stopSelf()
+        stopSelf()
     }
 
     override fun onBind(intent: Intent): IBinder {
+        Log.v(TAG,"onBind")
         TODO("Return the communication channel to the service.")
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-       msg()
+        Log.v(TAG,"onStartCommand")
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
+        Log.v(TAG,"onDestory")
+        super.onDestroy()
+        _fusedLocationClient.removeLocationUpdates(_onUpdateLocation)
 
     }
 
